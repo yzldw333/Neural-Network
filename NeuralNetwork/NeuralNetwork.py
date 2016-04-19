66fe699b634c7e77840421e8a14a87c41650af11
yzldw333 <yzldw333@gmail.com>
2016-04-19 15:16:43 +0800

    Add some comment.

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -28,27 +28,58 @@ class NeuralNetwork:
         self.setTest(self.samples_x[cvd_m:,:],self.samples_y[cvd_m:,:])
 
     def setTrain(self,train_x,train_y):
+        '''
+            Set training data, apply the training data to the model.
+        :param train_x: Training input x
+        :param train_y: Training output y
+        :return: None
+        '''
         self.train_x = train_x
         self.train_y = train_y
         self.model.SetTrainSamples(self.train_x,self.train_y)
 
     def setCVD(self,cvd_x,cvd_y):
+        '''
+            Set cross validation data, apply the cross validation data to the model's test data.
+        :param cvd_x: Cross validation input x
+        :param cvd_y: Cross validation output y
+        :return: None
+        '''
         self.cvd_x = cvd_x
         self.cvd_y = cvd_y
         self.model.SetTestSamples(self.cvd_x,self.cvd_y)
         pass
 
     def setTest(self,test_x,test_y):
+        '''
+            Set test data, for final test.
+        :param test_x: Test input x
+        :param test_y: Test output y
+        :return: None
+        '''
         self.test_x = test_x
         self.test_y = test_y
 
     def setFinalTest(self):
-        X = np.vstack(self.train_x,self.cvd_x)
-        Y = np.vstack(self.train_y,self.cvd_y)
+        '''
+            Apply training and cross validation data to model's training data.
+            Apply test data to model's test data.
+        :return: None
+        '''
+        X = np.vstack((self.train_x,self.cvd_x))
+        Y = np.vstack((self.train_y,self.cvd_y))
         self.model.SetTrainSamples(X,Y)
         self.model.SetTestSamples(self.test_x,self.test_y)
 
     def MiniBatch_Train(self,batch_size,steps,ifshow=True,gradiantCheck=False):
+        '''
+            A function for trainning in minibatch size
+        :param batch_size:      Num of training data in one iteration
+        :param steps:           Total training steps
+        :param ifshow:          True if you want to print real-time info in console
+        :param gradiantCheck:   False by default.
+        :return:                True if the training meet no error.
+        '''
         res = self.model.minibatch_train(batch_size,steps,ifshow,gradiantCheck)
         if res is False:
             print('Trainning Error!')
@@ -56,6 +87,15 @@ class NeuralNetwork:
         self.model.test_error()
 
     def Final_Train_and_Evaluate(self,batch_size,steps,ifshow=False):
+        '''
+            A function for final trainning.
+            In this function, the system will use training and validation data to train your neural network model,
+             and use test data to test error.
+        :param batch_size:      Num of training data in one iteration
+        :param steps:           Total training steps
+        :param ifshow:          True if you want to print real-time info in console
+        :return:                True if the training meet no error.
+        '''
         self.setFinalTest()
         self.MiniBatch_Train(batch_size,steps,ifshow)
 
